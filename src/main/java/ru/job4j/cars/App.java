483acaebe7bf920c5938475c5c7387aa91bebd61
spoletaev <info.elekit@yandex.ru>
2022-12-11 14:28:30 +0300

    создать mvn проект с начальной конфигурацией

---
+++ src/main/java/ru/job4j/cars/App.java
@@ -0,0 +1,13 @@
+package ru.job4j.cars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+        System.out.println("Go to http://localhost:8080/index");
+    }
+}
