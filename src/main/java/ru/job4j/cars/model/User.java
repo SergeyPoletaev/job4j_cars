8b317252612ff3576f03cf9ec49a169dcc35b278
spoletaev <info.elekit@yandex.ru>
2022-12-12 23:55:43 +0300

    2. HQL [#6874]

---
+++ src/main/java/ru/job4j/cars/model/User.java
@@ -0,0 +1,18 @@
+package ru.job4j.cars.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity(name = "auto_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String login;
+    private String password;
+}
