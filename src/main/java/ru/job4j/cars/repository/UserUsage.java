5629048c7577c27a40efaa6cfe9a9d1668cd8c52
spoletaev <info.elekit@yandex.ru>
2022-12-21 12:54:15 +0300

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/cars/repository/UserUsage.java
+++ src/main/java/ru/job4j/cars/repository/UserUsage.java
@@ -1,4 +1,4 @@
-package ru.job4j.cars.model.repository;
+package ru.job4j.cars.repository;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -10,7 +10,7 @@ public class UserUsage {
     public static void main(String[] args) {
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
         try (SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory()) {
-            var userRepository = new UserRepository(sf);
+            var userRepository = new UserRepository(new CrudRepository(sf));
             var user = new User();
             user.setLogin("admin5");
             user.setPassword("admin5");
