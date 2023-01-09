9f82894d2a9ae1dd280e2caf0603417d589cbd29
spoletaev <info.elekit@yandex.ru>
2023-01-09 15:52:00 +0300

    добавить хранение подписок на объявление
    1. добавить скрипт миграции
    2. связать таблицы БД и сущности через ManyToMany

--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -24,6 +24,13 @@ public class Post {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "auto_post_id")
     private List<PriceHistory> priceHistory = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = {@JoinColumn(name = "post_id")},
+            inverseJoinColumns = {@JoinColumn(name = "user_id")}
+    )
+    private List<User> participates = new ArrayList<>();
 
     @Override
     public boolean equals(Object o) {
