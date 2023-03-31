6bc9d004c734afbc9a96a9c27ab2ee31d8df100f
spoletaev <info.elekit@yandex.ru>
2023-03-31 08:48:14 +0300

    fix UserServiceImplTest

--- src/test/java/ru/job4j/cars/service/UserServiceImplTest.java
+++ src/test/java/ru/job4j/cars/service/UserServiceImplTest.java
@@ -12,62 +12,55 @@ class UserServiceImplTest {
     @Test
     void whenCreateThenProxy() {
         UserRepository userRepository = mock(UserRepository.class);
-        UserService userService = new UserServiceImpl(userRepository);
         User user = new User();
-        userService.create(user);
+        new UserServiceImpl(userRepository).create(user);
         verify(userRepository).create(user);
     }
 
     @Test
     void whenUpdateThenProxy() {
         UserRepository userRepository = mock(UserRepository.class);
-        UserService userService = new UserServiceImpl(userRepository);
         User user = new User();
-        userService.update(user);
+        new UserServiceImpl(userRepository).update(user);
         verify(userRepository).update(user);
     }
 
     @Test
     void whenDeleteThenProxy() {
         UserRepository userRepository = mock(UserRepository.class);
-        UserService userService = new UserServiceImpl(userRepository);
         int userId = 2;
-        userService.delete(userId);
+        new UserServiceImpl(userRepository).delete(userId);
         verify(userRepository).delete(userId);
     }
 
     @Test
     void whenFindAllOrderByIdThenProxy() {
         UserRepository userRepository = mock(UserRepository.class);
-        UserService userService = new UserServiceImpl(userRepository);
-        userService.findAllOrderById();
+        new UserServiceImpl(userRepository).findAllOrderById();
         verify(userRepository).findAllOrderById();
     }
 
     @Test
     void whenFindByIdThenProxy() {
         UserRepository userRepository = mock(UserRepository.class);
-        UserService userService = new UserServiceImpl(userRepository);
         int id = 1;
-        userService.findById(id);
+        new UserServiceImpl(userRepository).findById(id);
         verify(userRepository).findById(id);
     }
 
     @Test
     void whenFindByLikeLoginThenProxy() {
         UserRepository userRepository = mock(UserRepository.class);
-        UserService userService = new UserServiceImpl(userRepository);
         String key = "key";
-        userService.findByLikeLogin(key);
+        new UserServiceImpl(userRepository).findByLikeLogin(key);
         verify(userRepository).findByLikeLogin(key);
     }
 
     @Test
     void whenFindByLoginThenProxy() {
         UserRepository userRepository = mock(UserRepository.class);
-        UserService userService = new UserServiceImpl(userRepository);
         String login = "login";
-        userService.findByLogin(login);
+        new UserServiceImpl(userRepository).findByLogin(login);
         verify(userRepository).findByLogin(login);
     }
 }
